63cf4af6c53ab988691c5cc5ced45c8402b47a32
miguelkjesus <miguelkjesus05@gmail.com>
2023-12-10 23:33:01 +0000

    Lots of improvements

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Inspried by [this video](https://www.youtube.com/watch?v=NSS6yAMZF78)
 
+**WORKS BEST WITH STILL IMAGES**
+
 ## Setup
 
 1. Clone this repository.
@@ -13,6 +15,18 @@ pip install -r REQUIREMENTS.txt
 
 ## Usage
 
-```powershell
-py extract_motion.py <filename1> <filename2...> -o <offset> --out <outfolder/outfile>
 ```
+py extract_motion.py VIDEO_PATH -o FRAME_OFFSET [--out OUT] [--color] [--no-console] [--preview-video]
+```
+
+Positional Arguments
+
+- `VIDEO_PATH` **REQUIRED** The path to the video to process
+
+Options
+
+- `-o FRAME_OFFSET` **REQUIRED** The number of frames to delay the negative by. Larger offsets will track slower motion more effectively and vice versa.
+- `--out OUT` **REQUIRED** The path to save the processed video.
+- `--color` If set, output videos will keep their color, rather than be converted to grayscale. This will roughly double rendering times.
+- `--no-console` If set, the program will not print to the console.
+- `--preview-video` If set, displays a preview of the video as it is rendered. This will slow the rendering down a little bit.
--- extract_motion.py
+++ extract_motion.py
@@ -1,60 +1,133 @@
-import cv2, ntpath
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, cv2
+from typing import Any
 from argparse import ArgumentParser
 from collections import deque
+from tqdm import tqdm
+from termcolor import colored
 
-def main():
-    parser = ArgumentParser()
-    parser.add_argument('filepaths', nargs='+')
-    parser.add_argument('-o', '--offset', type=float, required=True)
-    parser.add_argument('--out')
-    args = parser.parse_args()
 
-    for filepath in args.filepaths:
-        video = cv2.VideoCapture(filepath)
+class ExtractMotion:
+    def __init__(self, video_path: str, frame_offset: int, out_path: str, *, grayscale: bool = True, no_console: bool = True, preview_video: bool = False) -> None:
+        self.video_path = video_path
+        self.frame_offset = frame_offset
+        self.out_path = out_path
+        self.grayscale = grayscale
+        self.no_console = no_console
+        self.preview_video = preview_video
+
+        self.PREVIEW_WINDOW_TITLE = "Press 'Q' to exit the preview" 
+
+    
+    def next_frame(self, video: cv2.VideoCapture):
+        ret, frame = video.read()
+        if self.grayscale:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        return ret, frame
+
+
+    def debug(self, string: str) -> None:
+        if not self.no_console:
+            print(string)
+
+
+    def get_motion_frame(self, frame1: Any, frame2: Any) -> Any:
+        processed = cv2.bitwise_not(cv2.add(frame1, cv2.bitwise_not(frame2)))
+        return processed
+
+
+    def start(self) -> str:
+        """ Processes the video and returns the path  """
+        video = cv2.VideoCapture(self.video_path)
 
         fps = int(video.get(cv2.CAP_PROP_FPS))
-        numFrames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-        invertedFrameDelay = int(args.offset * fps)
-
-        if invertedFrameDelay > numFrames:
-            raise ValueError(f"Offset too large. The offset must be less than the duration of the video.")
-
+        num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
         width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fourcc = int(video.get(cv2.CAP_PROP_FOURCC))
 
-        filenameNoExt = ntpath.basename(ntpath.splitext(filepath)[0])
-        outname = f"{filenameNoExt}-motion-extraction-offset-{args.offset}.mp4"
-        outdir = args.out or ntpath.dirname(filepath)
-        outpath = ntpath.join(outdir, outname)
+        if self.frame_offset > num_frames:
+            raise ValueError(f"Frame offset ({self.frame_offset}) must be < the number of frames in the video ({num_frames})")
 
         out = cv2.VideoWriter(
-            outpath,
-            cv2.VideoWriter_fourcc(*'avc1'),
+            self.out_path,
+            fourcc,
             fps,
-            (width, height)
-        )
+            (width, height),
+            0 if self.grayscale else None)
 
-        invertedQueue = deque()
-        while len(invertedQueue) < invertedFrameDelay:
-            _, frame = video.read()
-            invertedQueue.append(frame)
+        # Create queue of frames to offset the negative
+        inverted_queue = deque()
+        while len(inverted_queue) < self.frame_offset:
+            _, frame = self.next_frame(video)
+            inverted_queue.append(frame)
 
-        i = 0
-        while True:
-            ret, frame = video.read()
+        # Process video
+        self.debug(f"{colored('Processing', 'light_blue')} {colored(':', 'grey')} {os.path.abspath(self.video_path)}")
+
+        frame_idxs = range(0, num_frames - self.frame_offset)
+        for _ in tqdm(frame_idxs) if not self.no_console else frame_idxs:
+            ret, frame = self.next_frame(video)
             if not ret: break
+            
+            processed = self.get_motion_frame(frame, inverted_queue.popleft())
+            inverted_queue.append(frame)
+            out.write(processed)
 
-            print(f"Processing frame {i}")
-            inverted = cv2.bitwise_not(invertedQueue.popleft())
-            invertedQueue.append(frame)
-            frame = cv2.bitwise_not(cv2.add(frame, inverted))
-            out.write(frame)
+            if self.preview_video:
+                cv2.imshow(self.PREVIEW_WINDOW_TITLE, processed)
+                if cv2.waitKey(1) == ord("q"): 
+                    self.preview_video = False
+                    cv2.destroyWindow(self.PREVIEW_WINDOW_TITLE)
 
-            i += 1
+        self.debug(f"{colored('Saved in', 'green')} {colored(':', 'grey')} {os.path.abspath(self.out_path)}")
 
         video.release()
         out.release()
-    
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument(
+        'video_path',
+        help="The path to the video to process")
+    parser.add_argument(
+        '-o', 
+        '--frame-offset', 
+        type=int, 
+        required=True, 
+        help="The number of frames to delay the negative by. Larger offsets will track slower motion more effectively and vice versa.")
+    parser.add_argument(
+        '--out', 
+        help="The path to store the output file in")
+    parser.add_argument(
+        '--color', 
+        action='store_true', 
+        help="Doesn't convert to output to grayscale. (About half as fast as normal)")
+    parser.add_argument(
+        '--no-console', 
+        action='store_true',
+        help="If set, the program will not print to the console")
+    parser.add_argument(
+        '--preview-video', 
+        action='store_true',
+        help="If set, displays a preview of the video as it is rendered. This will slow the rendering down a little bit")
+    args = parser.parse_args()
+
+    path = os.path.splitext(args.video_path)[0]  # remove extension
+    out_path = args.out or f"{path}-frame-offset-{args.offset}.mp4"
+
+    ExtractMotion(
+        args.video_path, 
+        args.frame_offset, 
+        out_path, 
+        grayscale=not args.color,
+        no_console=args.no_console,
+        preview_video=args.preview_video
+    ).start()
+
 
 if __name__ == "__main__":
     main()
